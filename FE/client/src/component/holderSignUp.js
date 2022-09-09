065c4ba410d69f37019ddaecac9f2a7cd7b66d49
HoneyB <qudgus9601@naver.com>
2022-09-10 06:35:57 +0900

    update UI (not fin)

--- FE/client/src/component/holderSignUp.js
+++ FE/client/src/component/holderSignUp.js
@@ -44,24 +44,34 @@ const HolderSignUp = () => {
 
   return (
     <div>
-      <div>개인 회원 가입</div>
-      <div>개인 회원으로 가입하는 회원분들께서는</div>
-      <div>
-        인증서를 직접 관리할 수 있고, 원하는 인증서를 제출하는 서비스를
-        제공받습니다.
+      <div className="holdersignup--title">🙋‍♀️ 개인 회원 가입</div>
+      <div className="holdersignup--description">
+        <div>개인 회원으로 가입 시 인증서를 직접 관리할 수 있고</div>
+        <div>원하는 인증서를 제출하는 서비스를 제공받습니다.</div>
       </div>
 
       <Row>
         <Col span={6}>이메일</Col>
         <Col span={18}>
-          <input type="text" onChange={onchange} id="email" />
+          <input
+            type="text"
+            onChange={onchange}
+            id="email"
+            className="holdersignup--input"
+            placeholder="holdername@naver.com"
+          />
         </Col>
       </Row>
 
       <Row>
         <Col span={6}>비밀번호</Col>
         <Col span={18}>
-          <input type="password" onChange={onchange} id="password" />
+          <input
+            type="password"
+            onChange={onchange}
+            id="password"
+            className="holdersignup--input"
+          />
         </Col>
       </Row>
 
@@ -75,25 +85,42 @@ const HolderSignUp = () => {
                 ? setIsCorrect(true)
                 : setIsCorrect(false);
             }}
+            className="holdersignup--input"
           />
         </Col>
       </Row>
       <Row>
         <Col span={6}>이름</Col>
         <Col span={18}>
-          <input type="text" onChange={onchange} id="username" />
+          <input
+            type="text"
+            onChange={onchange}
+            id="username"
+            className="holdersignup--input"
+          />
         </Col>
       </Row>
       <Row>
         <Col span={6}>지갑 주소</Col>
         <Col span={18}>
-          <input type="text" onChange={onchange} id="walletAddress"></input>
+          <input
+            type="text"
+            onChange={onchange}
+            id="walletAddress"
+            className="holdersignup--input"
+          />
         </Col>
       </Row>
       <Row>
         <Col span={6}>생년월일</Col>
         <Col span={18}>
-          <input type="text" onChange={onchange} id="birth" placeholder="" />
+          <input
+            type="text"
+            onChange={onchange}
+            id="birth"
+            placeholder=""
+            className="holdersignup--input"
+          />
         </Col>
       </Row>
 
