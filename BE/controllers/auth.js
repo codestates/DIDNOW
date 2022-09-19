fc9b57a112048ab7fc5933b2f18fb95e65ff9e4e
ys <ys.heo.korea@gmail.com>
2022-09-19 15:18:56 +0900

    Issuer DID 분리

--- BE/controllers/auth.js
+++ BE/controllers/auth.js
@@ -89,7 +89,6 @@ const registerHolder = async (req, res, next) => {
     
     //Holder의 지갑 생성
     const {WalletPublicKey,WalletPrivateKey} = genWallet();
-    
 
     const newHolder = new Holder({ ...req.body, password: hashedPassword, walletAddress : WalletPublicKey });
 
