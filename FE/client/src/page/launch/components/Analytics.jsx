f0f48af7eed817e376bafc38549045700fa51af9
ys <ys.heo.korea@gmail.com>
2022-09-27 12:26:13 +0900

    MODIFY landing

--- FE/client/src/page/launch/components/Analytics.jsx
+++ FE/client/src/page/launch/components/Analytics.jsx
@@ -2,65 +2,51 @@ import React from "react";
 import Laptop from "img/homepage.jpg";
 import signin from "img/signin.jpg";
 import signup from "img/signup.jpg";
+import "../style/style.css";
 
 const Analytics = () => {
   return (
     <div className="w-full bg-white py-24  px-4">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img className="w-[400px] px-2 mx-auto my-4" src={Laptop} alt="/" />
-        <div className="flex flex-col justify-center">
-          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2">
-            발급 증명, 인증서 관리
+      <div className="max-w-[1000px] mx-auto grid md:grid-cols-2 landing-cards">
+        <img className="w-[400px] px-2 mx-auto my-4" src={Laptop} alt="/" style={{borderRadius:"30px"}}/>
+        <div className="w-[400px] flex flex-col justify-center">
+          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2 mb-7">
+            효율적인 인증서 관리
           </h1>
           <p className="md:text-xl sm:text-lg text-md">
-            블록체인 기술을 기반으로 한 웹을 통해 신뢰성을 갖추어, 기업에서
-            발급한 증명, 인증서를 관리할 수 있습니다.
+          <span style={{fontWeight:700}}>DIDNOW</span>는 블록체인 기술을 기반으로 한 웹을 통해 신뢰성을 갖추어, 기업에서 발급한 증명서/인증서를 제3자 간섭없이 관리할 수 있습니다.
           </p>
-          <p className="md:text-xl sm:text-lg text-sm">
-            본 서비스에서는 개인 혹은 단체에게 잘못 발급된 증명, 인증서도 삭제가
-            가능합니다.
-          </p>
-          {/* <button className="bg-black text-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
-            Get Started
-          </button> */}
         </div>
       </div>
-      <div className="max-w-[1240px] mt-20 mx-auto grid md:grid-cols-2">
-        <div className="flex flex-col justify-center">
-          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2">
-            발급 증명, 인증서 관리
+      <div className="max-w-[1000px] mt-20 mx-auto grid md:grid-cols-2  landing-cards">
+        <div className="w-[400px] flex flex-col justify-center">
+          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2 mb-7">
+            위변조가 불가능한 인증서 발급
           </h1>
           <p className="md:text-xl sm:text-lg text-md">
-            블록체인 기술을 기반으로 한 웹을 통해 신뢰성을 갖추어, 기업에서
-            발급한 증명, 인증서를 관리할 수 있습니다.
+            이제 인증서의 위변조를 걱정하지 않아도 됩니다. 블록체인을 통해 발급한 인증서의 진위 여부를 <span style={{fontWeight:700}}>DIDNOW</span>를 통해 간단하게 판별할 수 있습니다. 
           </p>
+          <br />
           <p className="md:text-xl sm:text-lg text-md">
-            본 서비스에서는 개인 혹은 단체에게 잘못 발급된 증명, 인증서도 삭제가
-            가능합니다.
+            간단한 절차로 사용자에게 100% 신뢰할 수 있는 인증서를 발급해 줄 수 있습니다.
           </p>
-          {/* <button className="bg-black text-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
-            Get Started
-          </button> */}
+          
         </div>
-        <img className="w-[400px] px-2 mx-auto my-4" src={signin} alt="/" />
+        <img className="w-[400px] px-2 mx-auto my-4" src={signin} alt="/" style={{borderRadius:"30px"}}/>
       </div>
-      <div className="max-w-[1240px] mt-20 mx-auto grid md:grid-cols-2">
-        <img className="w-[400px] px-2 mx-auto my-4" src={signup} alt="/" />
+      <div className="max-w-[1000px] mt-20 mx-auto grid md:grid-cols-2  landing-cards">
+        <img className="w-[400px] px-2 mx-auto my-4" src={signup} alt="/" style={{borderRadius:"30px"}}/>
         <div className="flex flex-col justify-center">
-          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2">
-            발급 증명, 인증서 관리
+          <h1 className="md:text-3xl sm:text-2xl text-1xl font-bold py-2 mb-7">
+            인증서 통합 관리 서비스
           </h1>
           <p className="md:text-xl sm:text-lg text-md">
-            블록체인 기술을 기반으로 한 웹을 통해 신뢰성을 갖추어, 기업에서
-            발급한 증명, 인증서를 관리할 수 있습니다.
+          <span style={{fontWeight:700}}>DIDNOW</span>는 블록체인 기술을 활용해 증명서, 자격증 등 다양한 인증서를 한번에 관리할 수 있습니다. 
           </p>
+          <br />
           <p className="md:text-xl sm:text-lg text-md">
-            본 서비스에서는 개인 혹은 단체에게 잘못 발급된 증명, 인증서도 삭제가
-            가능합니다.
+            인증서를 검증하고 관리하는데 복합하고 불필요한 비용을 지불할 필요가 없습니다.
           </p>
-          {/* <button className="bg-black text-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
-            Get Started
-          </button> */}
         </div>
       </div>
     </div>
--- FE/client/src/page/launch/components/Cards.jsx
+++ FE/client/src/page/launch/components/Cards.jsx
@@ -3,22 +3,30 @@ import Double from "img/double.png";
 import Triple from "img/triple.png";
 
 const Cards = () => {
+
+  const handleClick = () => {
+    window.location.href = '/signup'
+  }
+
   return (
     <div className="w-full py-[5rem] px-4 bg-white">
-      <p className="flex justify-center md:text-xl text-sm text-black mb-2">
-        DID NOW 는, 블록체인 기반의
-      </p>
-      <p className="flex justify-center md:text-xl text-sm text-black mb-20">
+      <h1 className="flex justify-center md:text-xl text-lg text-black mb-5" style={{fontSize:"30px"}}>
+        <span style={{fontWeight:700}}>DIDNOW</span>는 블록체인 기반의
+      </h1>
+      <h1 className="flex justify-center md:text-xl text-lg text-black mb-10" style={{fontSize:"30px"}}>
         비정부기관 증명 인증서 중개 플랫폼 입니다.
-      </p>
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 gap-8">
+      </h1>
+      <h1 className="flex justify-center md:text-xl text-lg text-black mb-20" style={{fontSize:"30px", fontWeight:"700"}}>
+      🚀️ 신뢰할 수 있는 인증을 경험하세요.
+      </h1>
+      <div className="max-w-[1240px] mx-auto flex gap-8">
         <div className="w-[300px] border-solid border-2 border-[#ffffff82] mx-auto shadow-2xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20  mx-auto mt-[-3rem] bg-transparent"
             src={Double}
             alt="/"
           />
-          <h2 className="text-2xl font-bold text-center py-8">개인</h2>
+          <h2 className="text-2xl font-bold text-center py-8">Holder</h2>
           <p className="text-center text-m ">신뢰성을 갖춘 증명서를</p>
           <p className="text-center text-m ">
             자유롭게 등록 및 관리 할 수 있습니다.
@@ -28,7 +36,7 @@ const Cards = () => {
             <p className="py-2 border-b mx-8 ">인증서 관리</p>
             <p className="py-2 border-b mx-8">인증서 등록</p>
           </div>
-          <button className="bg-[#0efcfe] text-black w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+          <button className="bg-[#0efcfe] text-black w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" onClick={handleClick}>
             회원가입
           </button>
         </div>
@@ -38,16 +46,35 @@ const Cards = () => {
             src={Triple}
             alt="/"
           />
-          <h2 className="text-2xl font-bold text-center py-8">기업</h2>
+          <h2 className="text-2xl font-bold text-center py-8">Issuer</h2>
           <p className="text-center text-m ">
             DID 기술을 통해 안전하게 기업의 인증서 발급 및 관리를 할 수있습니다.
           </p>
           <div className="text-center font-bold">
             <p className="py-2 border-b mx-8 mt-8">발급한 인증서 조회</p>
-            <p className="py-2 border-b mx-8">인증서 발급</p>
+            <p className="py-2 border-b mx-8">인증서 대상 관리</p>
             <p className="py-2 border-b mx-8 ">인증서 관리</p>
           </div>
-          <button className="bg-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+          <button className="bg-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" onClick={handleClick}>
+            회원가입
+          </button>
+        </div>
+        <div className="w-[300px] mx-auto shadow-2xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Verifier</h2>
+          <p className="text-center text-m ">
+            블록체인 DID를 통해 사용자의 자격증명을 효율적으로 검증할 수 있습니다.
+          </p>
+          <div className="text-center font-bold">
+            <p className="py-2 border-b mx-8 mt-8">발급한 인증서 조회</p>
+            <p className="py-2 border-b mx-8">자격검증 요청 관리</p>
+            <p className="py-2 border-b mx-8 ">인증서 관리</p>
+          </div>
+          <button className="bg-[#0efcfe] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3" onClick={handleClick}>
             회원가입
           </button>
         </div>
